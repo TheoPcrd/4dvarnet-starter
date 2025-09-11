6cd9ca4918842737d9e695b6d13a1ee94f60035a
Theo PICARD <t22picar@sl-mee-br-202.imta.fr>
2025-09-11 15:25:52 +0200

    Add lat

--- launch_main.sh
+++ launch_main.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --partition=Odyssey                         # Partition name
 #SBATCH --gres=gpu:l40s:1 #gpu:rtx8000:1 #gpu:h100:1 #gpu:rtx8000:1 #gpu:h100:1 # #gpu:h100:1 #gpu:rtx8000:1 #gpu:h100:1 #gpu:l40s:1  #gpu:l40s:1 #gpu:a100:1 #gpu:h100:1 #gpu:l40s:1 #gpu:a100:1 #gpu:l40s:1 #gpu:h100:1 #gpu:l40s:4 #gpu:h100:2 #gpu:h100:2 #gpu:rtx8000:3 #gpu:rtx8000:3  #gpu:a40:3 #gpu:h100:2 #gpu:rtx8000:3          # GPU request
-#SBATCH --job-name=multivar_8th                      # Job name
+#SBATCH --job-name=multivar_drift_4th_lat                      # Job name
 #SBATCH --cpus-per-gpu=12    # 12 CPUs for each GPU
 #SBATCH --output=log/job_%j.log # Standard output and error log (%j for jobid)
-#SBATCH --mem=490G #200G #180G #200G # #200G #180G #250G #480G #180G #300G #180G #480G #180G #200G #480G
+#SBATCH --mem=400G #200G #180G #200G # #200G #180G #250G #480G #180G #300G #180G #480G #180G #200G #480G
 
 export HOME=/Odyssey/private/t22picar/
 source "/Odyssey/private/t22picar/miniforge3/etc/profile.d/conda.sh"
 conda activate 4dvarnet-daniel
 
 #xp_name='unet_uv_drifters_cmems_15m_10y_11d_sla'
-#xp_name='unet_uv_drifters_cmems_15m_10y_11d_ugos'
-xp_name='unet_uv_drifters_cmems_15m_10y_11d_8th'
+xp_name='unet_uv_drifters_cmems_15m_10y_11d_ugos_lat'
+#xp_name='unet_uv_glorys_15m_10y_wind_11d_lat'
 #xp_name='unet_uv_glorys_15m_2y_wind_11d_8th'
 
 echo $xp_name
