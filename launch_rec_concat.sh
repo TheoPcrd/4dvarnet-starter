de9d666356cdabdda1e569de464ca4d531c82e89
Theo PICARD <t22picar@sl-mee-br-202.imta.fr>
2025-09-12 12:45:15 +0200

    config 21d 7d

--- launch_rec_concat.sh
+++ launch_rec_concat.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 #SBATCH --partition=Odyssey                         # Partition name
-#SBATCH --gres=gpu:h100:1 #gpu:rtx8000:1 #gpu:a40:3 #gpu:rtx8000:3 #gpu:l40s:4  #gpu:a40:3  #gpu:h100:2  #                           # GPU request
-#SBATCH --job-name=rec_uv                        # Job name
+#SBATCH --gres=gpu:rtx8000:1 #gpu:h100:1 #gpu:rtx8000:1 #gpu:h100:1 #gpu:rtx8000:1 #gpu:h100:1 #gpu:rtx8000:1 #gpu:a40:3 #gpu:rtx8000:3 #gpu:l40s:4  #gpu:a40:3  #gpu:h100:2  #                           # GPU request
+#SBATCH --job-name=rec_uv_glorys                        # Job name
 #SBATCH --cpus-per-gpu=12    # 12 CPUs for each GPU
 #SBATCH --output=log/job_%j.log # Standard output and error log (%j for jobid)
-#SBATCH --mem=180G
+#SBATCH --mem=100G
 
 export HOME=/Odyssey/private/t22picar/
 source "/Odyssey/private/t22picar/miniforge3/etc/profile.d/conda.sh"
 conda activate 4dvarnet-daniel
 import hydra
 
-HYDRA_FULL_ERROR=1 srun python main.py xp='ose_pipeline_1y_global_4_multivar_15m_unet_1patch_test_L4' +params='ose_pipeline_1y_1'
-
-HYDRA_FULL_ERROR=1 srun python main.py xp='ose_pipeline_1y_global_4_multivar_15m_unet_1patch_test_L4' +params='ose_pipeline_1y_2'
-
 # Chemin vers votre fichier YAML
 YAML_FILE="/Odyssey/private/t22picar/multivar_uv/config/xp/ose_pipeline_1y_global_4_multivar_15m_unet_1patch_test_L4.yaml"
 # Utiliser grep et awk pour extraire la valeur de xp_name
 xp_name=$(grep -m 1 'xp_name:' "$YAML_FILE" | awk '{print $2}')
+echo $xp_name
+
+HYDRA_FULL_ERROR=1 srun python main.py xp='ose_pipeline_1y_global_4_multivar_15m_unet_1patch_test_L4' +params='ose_pipeline_1y_1'
+
+HYDRA_FULL_ERROR=1 srun python main.py xp='ose_pipeline_1y_global_4_multivar_15m_unet_1patch_test_L4' +params='ose_pipeline_1y_2'
 
 #cd rec/
 srun python concat_rec_saving_filter.py "$xp_name" # 
